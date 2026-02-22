2749a39542814b55793dea4efc8e74d5efe2f77f
max_ksiazka <maksymilian.ksiazka@gmail.com>
2026-02-22 20:13:10 +0100

    Refactor: Change variable names from Hungarian notation, variable type to fixed-width integers

--- inc/VL53L0X_internal.h
+++ inc/VL53L0X_internal.h
@@ -3,17 +3,18 @@
 
 #ifndef __TOF_LIBRARY_INTERNAL__
 #error                                                                         \
-    "VL53L0X_internal.h is for internal use only. Do not include it in your code."
+    "VL53L0X_internal.h is for internal use only. Do not include it in your code. If you need to use it, define __TOF_LIBRARY_INTERNAL__ before including it."
 #endif
 #include "VL53L0X_lib.h"
 
-uint8_t readReg(tof_device_t* dev, uint8_t ucAddr);
-uint16_t readReg16(tof_device_t* dev, uint8_t ucAddr);
-int32_t writeReg(tof_device_t* dev, uint8_t ucAddr, uint8_t ucValue);
-void writeReg16(tof_device_t* dev, uint8_t ucAddr, uint16_t usValue);
-void writeRegList(tof_device_t* dev, uint8_t* ucList);
-void readMulti(tof_device_t* dev, uint8_t ucAddr, uint8_t* pBuf,
-               int32_t iCount);
-void writeMulti(tof_device_t* dev, uint8_t ucAddr, uint8_t* pBuf,
-                int32_t iCount);
+uint8_t readReg(tof_device_t* dev, uint8_t reg_addr);
+int32_t writeReg(tof_device_t* dev, uint8_t reg_addr, uint8_t reg_val);
+uint16_t readReg16(tof_device_t* dev, uint8_t reg_addr);
+int32_t writeReg16(tof_device_t* dev, uint8_t reg_addr, uint16_t reg_val);
+int32_t writeRegList(tof_device_t* dev, uint8_t* reg_addr_val_pair_list);
+void readMulti(tof_device_t* dev, uint8_t reg_addr, uint8_t* buffer,
+               int32_t count);
+int32_t writeMulti(tof_device_t* dev, uint8_t reg_addr, uint8_t* buffer,
+                   int32_t count);
+
 #endif // VL53L0X_INTERNAL_H
--- src/tof_register_functions_internal.c
+++ src/tof_register_functions_internal.c
@@ -1,32 +1,88 @@
 #define __TOF_LIBRARY_INTERNAL__
 #include "VL53L0X_internal.h"
-uint16_t readReg16(tof_device_t* dev, uint8_t ucAddr) {
-    uint8_t ucTemp[2];
-
-    dev->i2c_ops->i2c_write(dev->i2c_ops->user_ctx, dev->addr, &ucAddr, 1,
-                            true);
-    dev->i2c_ops->i2c_read(dev->i2c_ops->user_ctx, dev->addr, ucTemp, 2, false);
-    // Replaces:
-    // i2c_write_blocking(i2c_port, addr, &ucAddr, 1, true);
-    // i2c_read_blocking(i2c_port, addr, ucTemp, 2, false);
-    return (uint16_t)((ucTemp[0] << 8) + ucTemp[1]);
-} /* readReg16() */
 
 //
 // Read a single register value from I2C device
 //
-uint8_t readReg(tof_device_t* dev, uint8_t ucAddr) {
-    uint8_t ucTemp;
-    dev->i2c_ops->i2c_write(dev->i2c_ops->user_ctx, dev->addr, &ucAddr, 1,
+uint8_t readReg(tof_device_t* dev, uint8_t reg_addr) {
+    uint8_t reg_buf;
+    dev->i2c_ops->i2c_write(dev->i2c_ops->user_ctx, dev->addr, &reg_addr, 1,
                             true);
-    dev->i2c_ops->i2c_read(dev->i2c_ops->user_ctx, dev->addr, &ucTemp, 1,
+    dev->i2c_ops->i2c_read(dev->i2c_ops->user_ctx, dev->addr, &reg_buf, 1,
                            false);
     // Replaces:
     // i2c_write_blocking(i2c, addr, &ucAddr, 1, true);
     // i2c_read_blocking(i2c, addr, &ucTemp, 1, false);
 
-    return ucTemp;
+    return reg_buf;
 } /* ReadReg() */
+//
+// Write a single register/value pair
+//
+int32_t writeReg(tof_device_t* dev, uint8_t reg_addr, uint8_t reg_val) {
+    uint8_t payload[2];
+
+    payload[0] = reg_addr;
+    payload[1] = reg_val;
+
+    int ret = dev->i2c_ops->i2c_write(dev->i2c_ops->user_ctx, dev->addr,
+                                      payload, 2, false);
+    // Replaces:
+    // i2c_write_blocking(i2c, addr, ucTemp, 2, false);
+
+    return ret;
+}
+
+uint16_t readReg16(tof_device_t* dev, uint8_t reg_addr) {
+    uint8_t payload[2];
+
+    dev->i2c_ops->i2c_write(dev->i2c_ops->user_ctx, dev->addr, &reg_addr, 1,
+                            true);
+    dev->i2c_ops->i2c_read(dev->i2c_ops->user_ctx, dev->addr, payload, 2, false);
+    // Replaces:
+    // i2c_write_blocking(i2c_port, addr, &ucAddr, 1, true);
+    // i2c_read_blocking(i2c_port, addr, ucTemp, 2, false);
+    return (uint16_t)((payload[0] << 8) + payload[1]);
+} /* readReg16() */
+//
+// Write a 16-bit value to a register
+//
+int32_t writeReg16(tof_device_t* dev, uint8_t reg_addr, uint16_t reg_val) {
+    uint8_t ucTemp[4];
+
+    ucTemp[0] = reg_addr;
+    ucTemp[1] = (uint8_t)(reg_val >> 8); // MSB first
+    ucTemp[2] = (uint8_t)reg_val;
+
+    int32_t ret = dev->i2c_ops->i2c_write(dev->i2c_ops->user_ctx, dev->addr,
+                                          ucTemp, 3, false);
+    // Replaces:
+    // i2c_write_blocking(i2c, addr, ucTemp, 3, false);
+    return ret;
+
+} /* writeReg16() */
+//
+// Write a list of register/value pairs to the I2C device
+//
+int32_t writeRegList(tof_device_t* dev, uint8_t* reg_addr_val_pair_list) {
+    uint8_t count = *reg_addr_val_pair_list++; // count is the first element in the list
+    int32_t bytes_sent = 0;
+    while (count) {
+        int32_t ret = dev->i2c_ops->i2c_write(dev->i2c_ops->user_ctx, dev->addr, reg_addr_val_pair_list, 2,
+                                false);
+        if (ret != 2) {
+            // Handle error (e.g., log it, retry, etc.)
+            // For now, we just break the loop on error
+            break;
+        }
+        bytes_sent += ret;
+        // Replaces:
+        // i2c_write_blocking(i2c, addr, ucList, 2, false);
+        reg_addr_val_pair_list += 2;
+        count--;
+    }
+    return bytes_sent;
+}
 
 void readMulti(tof_device_t* dev, uint8_t ucAddr, uint8_t* pBuf, int iCount) {
     dev->i2c_ops->i2c_write(dev->i2c_ops->user_ctx, dev->addr, &ucAddr, 1,
@@ -38,62 +94,15 @@ void readMulti(tof_device_t* dev, uint8_t ucAddr, uint8_t* pBuf, int iCount) {
     // i2c_read_blocking(i2c, addr, pBuf, iCount, false);
 } /* readMulti() */
 
-void writeMulti(tof_device_t* dev, uint8_t ucAddr, uint8_t* pBuf, int iCount) {
-    uint8_t ucTemp[16];
+int32_t writeMulti(tof_device_t* dev, uint8_t reg_addr, uint8_t* buffer, int count) {
+    uint8_t temp[16];
     int rc;
 
-    ucTemp[0] = ucAddr;
-    memcpy(&ucTemp[1], pBuf, iCount);
-    dev->i2c_ops->i2c_write(dev->i2c_ops->user_ctx, dev->addr, ucTemp,
-                            iCount + 1, false);
+    temp[0] = reg_addr;
+    memcpy(&temp[1], buffer, count);
+    int32_t ret = dev->i2c_ops->i2c_write(dev->i2c_ops->user_ctx, dev->addr, temp,
+                            count + 1, false);
     // Replaces:
     // i2c_write_blocking(i2c, addr, ucTemp, iCount + 1, false);
-} /* writeMulti() */
-//
-// Write a 16-bit value to a register
-//
-void writeReg16(tof_device_t* dev, uint8_t ucAddr, uint16_t usValue) {
-    uint8_t ucTemp[4];
-    int rc;
-
-    ucTemp[0] = ucAddr;
-    ucTemp[1] = (uint8_t)(usValue >> 8); // MSB first
-    ucTemp[2] = (uint8_t)usValue;
-
-    dev->i2c_ops->i2c_write(dev->i2c_ops->user_ctx, dev->addr, ucTemp, 3,
-                            false);
-    // Replaces:
-    // i2c_write_blocking(i2c, addr, ucTemp, 3, false);
-
-} /* writeReg16() */
-//
-// Write a single register/value pair
-//
-int32_t writeReg(tof_device_t* dev, uint8_t ucAddr, uint8_t ucValue) {
-    uint8_t ucTemp[2];
-    int rc;
-
-    ucTemp[0] = ucAddr;
-    ucTemp[1] = ucValue;
-    int ret = dev->i2c_ops->i2c_write(dev->i2c_ops->user_ctx, dev->addr, ucTemp,
-                                      2, false);
-    // Replaces:
-    // i2c_write_blocking(i2c, addr, ucTemp, 2, false);
-
     return ret;
-}
-
-//
-// Write a list of register/value pairs to the I2C device
-//
-void writeRegList(tof_device_t* dev, uint8_t* ucList) {
-    uint8_t ucCount = *ucList++; // count is the first element in the list
-    while (ucCount) {
-        dev->i2c_ops->i2c_write(dev->i2c_ops->user_ctx, dev->addr, ucList, 2,
-                                false);
-        // Replaces:
-        // i2c_write_blocking(i2c, addr, ucList, 2, false);
-        ucList += 2;
-        ucCount--;
-    }
-}
+} /* writeMulti() */
